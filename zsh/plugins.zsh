f5b6d23d2f13cf6a8f8f8e3b4652e077dd8a4d38
masimacbook <masi19bw@gmail.com>
2022-01-20 10:53:13 +0900

    add bat

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,3 +2,14 @@ zinit ice depth=1; zinit light romkatv/powerlevel10k
 zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light paulirish/git-open
+
+zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit light sharkdp/bat
+
+# 以下はただのエイリアス設定
+if builtin command -v bat > /dev/null; then
+  alias cat="bat"
+  alias less='bat --paging=always'
+fi
+
+
