e8d5110db84950561180b1336f99c0fbd202eed3
masibw <masi19bw@gmail.com>
2022-01-24 19:14:19 +0900

    fix: in m1 mac, we have to download via brew

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,9 +3,6 @@ zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light paulirish/git-open
 
-zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
-zinit light sharkdp/bat
-
 # 以下はただのエイリアス設定
 if builtin command -v bat > /dev/null; then
   alias cat="bat"
