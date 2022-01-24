3ad253f4676f156abb983272fde26304b5fc1b91
masibw <masi19bw@gmail.com>
2022-01-24 22:40:12 +0900

    add command alias, path

---
+++ zsh/config.zsh
@@ -0,0 +1,10 @@
+. /opt/homebrew/opt/asdf/libexec/asdf.sh
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+. ~/src/github.com/rupa/z/z.sh
+. ~/.asdf/plugins/java/set-java-home.zsh
+alias ls='exa -al'
+alias df='duf'
+alias find='fd'
+alias top='gotop'
